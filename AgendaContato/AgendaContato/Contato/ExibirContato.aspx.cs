6f6dce2ce9dff2c907f46cfcf39f550ca1a3f87a
Pedro Oliveira <96023606+PedroPro212@users.noreply.github.com>
2022-11-15 18:49:15 -0300

    Adição contagem de contatos (COUNT)

--- AgendaContato/AgendaContato/Contato/ExibirContato.aspx.cs
+++ AgendaContato/AgendaContato/Contato/ExibirContato.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,7 @@ namespace AgendaContato.Contato
 {
     public partial class ExibirContato : System.Web.UI.Page
     {
+        private MySqlConnection connection = new MySqlConnection(SiteMaster.ConnectionString);
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -20,6 +22,28 @@ namespace AgendaContato.Contato
             Session["dados"] = contato;
             grdContatos.DataSource = contato;
             grdContatos.DataBind();
+
+
+            var reader = new MySqlCommand("SELECT COUNT(nome) AS qts FROM adicionar", connection);
+            
+            try
+            {
+                connection.Open();
+                lblQts.Text = "";
+                MySqlDataReader dataReader;
+                dataReader = reader.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    lblQts.Text += dataReader.GetInt32("qts");
+                }
+            }
+            catch(Exception ex)
+            {
+                SiteMaster.AlertPersonalizado(this, ex.Message);
+            }
+            connection.Close();
+            
+
         }
 
         protected void btnCadastrar_Click(object sender, EventArgs e)
--- AgendaContato/AgendaContato/Contato/ExibirContato.aspx.designer.cs
+++ AgendaContato/AgendaContato/Contato/ExibirContato.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace AgendaContato.Contato
         protected global::System.Web.UI.WebControls.Button btnCadastrar;
 
         /// <summary>
+        /// Controle lblQts.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblQts;
+
+        /// <summary>
         /// Controle txtNome.
         /// </summary>
         /// <remarks>
